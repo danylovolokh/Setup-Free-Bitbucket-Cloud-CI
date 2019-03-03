de31660bedeb146001f1e362965a8bc8e5c87f8c
danylo.volokh <danylo.volokh@globallogic.com>
2019-03-03 14:12:43 +0200

    Initial Commit

---
+++ app/src/main/java/mots_mockup/comarch/com/setupbitbucketcloudci/MainActivity.java
@@ -0,0 +1,13 @@
+package mots_mockup.comarch.com.setupbitbucketcloudci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
