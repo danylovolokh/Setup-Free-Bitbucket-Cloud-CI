2da263d4697d9f6c5ec5d601d62614cd7f0d52eb
danylo.volokh <danylo.volokh@globallogic.com>
2019-03-10 11:45:17 +0200

    Update upload to Downloads

--- 6_upload_apk_file_to_bitbucket_downloads.py
+++ 6_upload_apk_file_to_bitbucket_downloads.py
@@ -9,10 +9,11 @@ build_number = ""
 application_version = ""
 # << fields from build_info.json
 
-def execute_command(error_message, git_commit):
+def execute_command(error_message, command):
+    print ("execute_command curl [%s]" % command)
     try:
 
-        codeproc = subprocess.Popen(git_commit, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        codeproc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         result = codeproc.stdout.read().decode("utf-8").strip()
         print(result)
 
@@ -47,7 +48,7 @@ def main(argv):
     os.rename('app/build/outputs/apk/external/release/app-external-release.apk', 'app/build/outputs/apk/external/release/%s_external.apk' % final_version_name)
     os.rename('app/build/outputs/apk/internal/release/app-internal-release.apk', 'app/build/outputs/apk/internal/release/%s_internal.apk' % final_version_name)
 
-    print ("%s" % final_version_name)
+    print ("final_version_name [%s]" % final_version_name)
 
     BB_AUTH_STRING = os.environ.get('BB_AUTH_STRING')
     BITBUCKET_REPO_OWNER = os.environ.get('BITBUCKET_REPO_OWNER')
@@ -57,8 +58,12 @@ def main(argv):
     filepath_external = 'app/build/outputs/apk/external/release/%s_external.apk' % final_version_name
     filepath_internal = 'app/build/outputs/apk/internal/release/%s_internal.apk' % final_version_name
 
+    print (">> execute_command curl filepath_external")
+
     curl_exec = ["curl", "-X", "POST", downloads__url, "--form", "files=@%s" % filepath_external]
     execute_command("failed to upload", curl_exec)
+
+    print (">> execute_command curl filepath_internal")
 
     curl_exec = ["curl", "-X", "POST", downloads__url, "--form", "files=@%s" % filepath_internal]
     execute_command("failed to upload", curl_exec)
